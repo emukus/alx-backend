4f4b8b244f06220f9f91117c6182853442e4f5f5
emukus <mukuna.eugene@gmail.com>
2023-02-20 11:08:23 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes two int aguments and returns a tuple of size two
+containing a start and end index corresponding to the range of indexes"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Params: page: page to be indexed
+    page_size: no. of indexes a page should have
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
